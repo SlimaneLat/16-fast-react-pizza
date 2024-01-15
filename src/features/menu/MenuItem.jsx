05ea3e3af3df1262c36e32d093c17aba98e6e61b
slimpark <slimanelatreche94@gmail.com>
2024-01-14 23:28:24 -0800

    creating components and routes using modern react routes

---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../utils/helpers";
+import PropTypes from "prop-types";
+
+MenuItem.propTypes = {
+  pizza: PropTypes.object,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  unitPrice: PropTypes.string,
+  ingredients: PropTypes.object,
+  soldOut: PropTypes.bool,
+  imageUrl: PropTypes.string,
+};
+
+function MenuItem({ pizza }) {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  console.log(id + "MenuItem.jsx");
+
+  return (
+    <li>
+      <img src={imageUrl} alt={name} />
+      <div>
+        <p>{name}</p>
+        <p>{ingredients.join(", ")}</p>
+        <div>
+          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Slimane latreche</p>
+    </header>
+  );
+}
+
+export default Header;
